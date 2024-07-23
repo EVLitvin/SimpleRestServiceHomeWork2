4ccf225f7c4aa6e50b792e9d309366d5c4742f13
LitvinEV <ev_litvin@yahoo.com>
2024-07-23 11:38:20 +0500

    Add DTOs.

---
+++ src/main/java/ru/evlitvin/dto/SchoolDto.java
@@ -0,0 +1,51 @@
+package ru.evlitvin.dto;
+
+import java.util.List;
+
+public class SchoolDto {
+
+    private Long id;
+    private String name;
+    private String address;
+    private List<TeacherDto> teachers;
+
+    public SchoolDto() {}
+
+    public SchoolDto(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<TeacherDto> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<TeacherDto> teachers) {
+        this.teachers = teachers;
+    }
+}
